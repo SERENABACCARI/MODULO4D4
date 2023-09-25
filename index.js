ef94fe766e2fc727cb30649825d0e6c9492605c2
SERENABACCARI <serena88_2011@libero.it>
2023-09-25 16:07:15 +0200

    m4d4

--- index.js
+++ index.js
@@ -1,8 +1,34 @@
-const url = "https://striveschool-api.herokuapp.com/books"
-
+/* url = "https://striveschool-api.herokuapp.com/books"*/
 window.onload=()=>{
-    fetchbooks()
+    fetch()
 }
+fetch("https://striveschool-api.herokuapp.com/books")
+.then(response=>response.json())
+.then((response)=>{
+    let cont=document.querySelector(".libri .row")
+    cont.innerHTML=response.map((book)=>{
+        return `<div class="col-12 col-sm-6 col-md-4 col-lg-2"><div class="card"id='book_${book.asin}'>
+            <img src="${book.img}" class="card-img-top" alt="">
+            <div class="card-body">
+                 <button class='btn btn-primary' onclick="addToCart('${book.title}', '${book.price}', '${book.asin}')"> EUR ${book.price} </button>
+                <a href="#" class="btn btn-primary">saltare</a>
+            </div>
+        </div>
 
-fetch(url)
+        </div>`
+    })
+        .join("")
+})
+    .catch((err) => console.error(err))
+    
 
+
+/*funzione carrello*/
+
+let carrello=[]
+
+function addtocart(book){
+    carrello.push(book)
+    document.getElementById("numero prodotti").innerHTML = book.length
+
+}
